5174fe7476e08930160feb4d55d5cd7cc8604007
Anselm Schacher <anselm.schacher@kauz.ch>
2018-03-26 13:10:44 +0200

    Added links and correct headers to the rental table

--- app/Views/show.view.php
+++ app/Views/show.view.php
@@ -1,29 +1,31 @@
 <div class="container">
     <div class="row">
-        <div class="col-3"></div>
-        <div class="col-6">
+        <div class="col-2"></div>
+        <div class="col-8">
             <h3>Ausleihen anzeigen</h3>
             <table class="table">
                 <thead>
                     <tr>
                     <th scope="col">Name</th>
-                    <th scope="col">Surname</th>
-                    <th scope="col">Movie</th>
-                    <th scope="col">Date to return</th>
+                    <th scope="col">Vorname</th>
+                    <th scope="col">Film</th>
+                    <th scope="col">Rückgabedatum</th>
+                    <th scope="col"></th>
                     </tr>
                 </thead>
                 <tbody>
                 <?php foreach ($rentals as $rental) { ?>
                     <tr>
-                    <th scope="row"><?= $rental["name"] ?></th>
+                    <td><?= $rental["name"] ?></td>
                     <td><?= $rental["surname"] ?></td>
                     <td><?= $rental["title"] ?></td>
                     <td><?= $rental["time_return"] ?></td>
+                    <td><a href="edit?id=<?= $rental["RentalID"]?>">Bearbeiten</a></td>
                 <?php } ?>
                 </tbody>
             </table>
         </div>
-        <div class="col-3"></div>
+        <div class="col-2"></div>
 
     </div>
 </div>
